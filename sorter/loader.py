da1452a08da7c020d7a882337d2781a182873ff8
Student <gvb@gmail.com>
2021-02-08 19:08:17 +0400

    Дописал документацию.

---
+++ sorter/loader.py
@@ -0,0 +1,27 @@
+"""
+Загрузка данных.
+"""
+def _str_to_int_list(raw_list):
+    return [int(i) for i in raw_list.split()]
+
+def load_from_input() -> list:
+    '''Получение данных через input
+    :return: список несортированных данных'''
+    while True:
+        try:
+            raw_list = input('Введие числа разделенные пробелом: ')
+            result = _str_to_int_list(raw_list)
+        except ValueError:
+            print('Вы ввели неправельное число')
+        else:
+            return result
+
+
+def load_from_file(filename: str) -> list:
+    '''Получение данных из файла
+    :param filename: имя фйла
+    :return: список несортированных данных'''
+    with open(filename) as file:
+        raw_list = file.read()
+    result = _str_to_int_list(raw_list)
+    return result
